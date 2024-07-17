e42d14896435352c4dccfbac192d02c3e0e55366
Ljazz <mxm41121@163.com>
2024-07-17 09:59:00 +0800

    聚合配置变量

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     // 通过 env::args() 方法读取分析传入的命令行参数
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -21,9 +21,11 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config { query, file_path }
+        Config { query, file_path }
+    }
 }
