80ca3485b8607433ba82d42c4f79a2e65272e3ef
Ljazz <mxm41121@163.com>
2024-07-17 09:50:50 +0800

    分离命令行解析

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ fn main() {
     // 通过 env::args() 方法读取分析传入的命令行参数
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
