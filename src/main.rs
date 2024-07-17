ad2739e52434aa5eb2def568b61a42666b8e9a39
Ljazz <mxm41121@163.com>
2024-07-17 11:01:28 +0800

    使用迭代器优化代码

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::{env, process};
 
 fn main() {
     // 通过 env::args() 方法读取分析传入的命令行参数
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
