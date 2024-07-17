20c74022c5767ed928bd62540701ea62511cda12
Ljazz <mxm41121@163.com>
2024-07-17 10:10:15 +0800

    1. 分离主体逻辑到run中 2. 使用?和特征对象来返回错误 3. 处理错误

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     // 通过 env::args() 方法读取分析传入的命令行参数
@@ -12,11 +12,18 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    // 文件内容读取
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
 
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // 文件内容读取
+    let contents = fs::read_to_string(config.file_path)?;
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
