c65068c64f7b152971e3b16911d587c1baa82c51
Ljazz <mxm41121@163.com>
2024-07-17 09:57:20 +0800

    1. 文件读取 2. 聚合配置变量

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     // 通过 env::args() 方法读取分析传入的命令行参数
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    // 文件内容读取
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
